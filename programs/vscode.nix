241fc024d012d774d032bd13fe1f004f8bdf299b
Spyrichie <rcc@richardchurch.com>
2025-08-18 14:49:59 -0500

    change the way VSCode is loaded

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -7,13 +7,11 @@
   users.users.${username}.packages = with pkgs; [
     (vscode-with-extensions.override {
       vscode = vscodium;
-      vscodeExtensions = with open-vsx; [
-        bbenoist.nix
-        ms-python.vscode-pylance
-        ms-python.python
-      ];
       vscodeExtensions = [
         nixpkgs-stable.vscode-extensions.devsense.phptools-vscode
+        open-vsx.bbenoist.nix
+        open-vsx.ms-python.vscode-pylance
+        open-vsx.ms-python.python
       ];
     })
   ];
