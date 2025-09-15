8912872da16e710a3addec6b452a0c2573e7c1ad
Spyrichie <spyrichie123@proton.me>
2025-09-15 15:05:00 -0500

    changes

--- user/rcc/home.nix
+++ user/rcc/home.nix
@@ -2,7 +2,7 @@
 {
   imports = [
     ../../modules/home/core.nix
-    ../../modules/home/desktop/wayland/hyprland
-    ../../modules/home/desktop/wayland/waybar
+    # ../../modules/home/desktop/wayland/hyprland
+    # ../../modules/home/desktop/wayland/waybar
   ];
 }
